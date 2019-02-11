426b3ccdce20c1bf898551b5693d2ccfa5e2b420
Хлоя <allenzj@cardiff.ac.uk>
2019-02-11 12:59:39 +0000

    Initial code commit, blockchain implemented from https://www.youtube.com/watch?v=1O-XnbRYJHM
    Modifications made to date:
    	-Updated block contents to include hashes from both guest controller and base unit, session objective, time of block creation and block serial number.
    	-int i to increment as serial number of blocks
    	-removed hackBlock function (superfluous to needs)
    	-Added notes to explain functions of code
    
    To Do:
    	-Test current code
    		-Must be able to get blockchain running by 20th Feb 19
    	-Consider methods of displaying data collected from base station
    	-Write code to create sessionObjectives code
    	-Write code to interact with the microcontroller (in the base station)
    		-Passing sessionObjectives to the microcontroller
    		-Passing completed blockchain ledger to/from microcontroller
    	-Write interaction code
    		-Radio/BLE elements need to be scanning then when confirmed connection must form blocks without prompting
    	-Write method to load UID onto visitors tags
    		-Python may be easiest to write/deploy

---
+++ blockChain.c
@@ -0,0 +1,297 @@
+#include "stdio.h"
+#include "openssl/crypto.h"
+#include "time"
+#include "stdlib.h"
+#include "string.h"
+
+
+// Create block structure
+struct block
+{
+	/*
+	Each block contains:
+		Hash number
+		integer called data
+		previous block link location
+
+	*/
+	// Must add Hashcode1, Hashcode2, currentTime, sessionObjectives, serialisationNumber
+	unsigned char prevHash[SHA256_DIGEST_LENGTH];
+	int data;
+	struct block *link;
+	unsigned char* ownHashID;
+	unsigned char* guestHashID;
+	time_t currentTime;
+	unsigned char* sessionObjectives;
+	int serialisationNumber;
+
+
+// *head is the header pointing to the next ref in the chain?
+}*head;
+// Adds new block
+void addBlock(int data, unsigned char* sessionObjectives, time_t currentTime);
+// verifys chain integrity
+void verifyChain();
+// alters the block at position n [redundent feature]
+void alterNthBlock(int n,int newData);
+// Compares hashes for validity checking
+int hashCompare(unsigned char*,unsigned char*);
+// Prints hash numbers
+void hashPrinter();
+// Turns unsigned char hashes into strings for SHA256 alg to interpret
+unsigned char* toString(struct block);
+// Print one block
+void printBlock();
+// Print all blocks
+void printAllBlocks();
+
+int i->1
+
+// SHA256(src, size, dest)
+// src and dest are type of unsigned char *
+
+// add a block to the chain
+// Need some help discussing the workings of this
+void addBlock(int data, unsigned char* sessionObjectives, time_t currentTime, int i)
+{
+	// If head is empty generate genesis block
+	if(head==NULL)
+	{
+		// Allocate an area of memory the size of a block
+		// head=malloc returns an address in memory
+		// head now equals a memory address
+		head=malloc(sizeof(struct block));
+		// Hash "" and the size of "" and store in previous hash location(retrived from head)
+		SHA256("",sizeof(""),head->prevHash);
+		// head should now read block data
+		head->blockData=data;
+		return;
+	}
+	int i++;
+	struct block *currentBlock=head;
+	// While there is a previous block (i.e blockchain is valid) do:
+	while(currentBlock->link)
+		// link between blocks
+		currentBlock=currentBlock->link;
+	// New block created in memory with malloc
+	struct block *newBlock=malloc(sizeof(struct block));
+	// New block gets linked to old block
+	currentBlock->link=newBlock;
+	// New blocks data is now at this memory address
+	// Add own data here
+	newBlock->blockData=data;
+	// Add extra data:
+	/*
+	time_t currentTime;
+	unsigned char sessionObjectives;
+	int serialisationNumber;
+	*/
+	// Add session objectives
+	newBlock->sessionObjectives = sessionObjectives;
+	serialisationNumber -> i = serialisationNumber;
+
+	// Generate hash of current block and old block?
+	SHA256(toString(*currentBlock),sizeof(*currentBlock),newBlock->prevHash);
+}
+
+// Need help explaining this one
+/*line 73 (for me, but it's where the hashPrinter call is).
+he generates the hash of the previous block, fine. doesn't store it because we aren't modifying or writing, just checking it against the value that is already stored. 
+He then prints the stored version on line 75 with the second call to hashPrinter. so you would be able to see that it matches or differes.
+
+However, his next line is to hashCompare, where he generates the hash of the previous block a second time. so he's repeating a step, so this part really takes twice as long as it needs to. 
+
+This could be modifed to remove the two calls to hashPrinter. Instead, it would be better to inside hashCompare print str1 and str2 in the same way to give you that visual comparison, you're then only generating it once. 
+
+equally, if it matches, do you really want it printed out? it would be better if it only printed it out when it failed, otherwise the screen will get very cluttered. This one is my opinion, and it depends on what you want from the program. if printing the valid data is useful to you then that's fine.
+*/
+void verifyChain()
+{
+	// If empty, say so
+	if(head==NULL)
+	{
+		printf("Blockchain is empty, add some blocks!\n");
+		return;
+	}
+	struct block *curr=head->link,*prev=head;
+	int count=1;
+	while(curr)
+	{
+		printf("%d\t[%d]\t\n",count++,curr->blockData);
+		hashPrinter(SHA256(toString(*prev),sizeof(*prev),NULL),SHA256_DIGEST_LENGTH);
+		printf(" - ");
+		hashPrinter(curr->prevHash, SHA256_DIGEST_LENGTH);
+		if(hashCompare(SHA256(toString(*prev),sizeof(*prev),NULL),curr->prevHash))
+			printf("Verified!\n");
+		else
+			printf("Verification Failed! :(\n");
+		prev=curr;
+		curr=curr->link;
+	}
+}
+// Alter the block at position n with new data
+// Should this be necessary?
+void alterNthBlock(int n, int newData)
+{
+// n = block position
+// newData = new data
+	// If head points to NULL, block is not valid
+	struct block *curr=head;
+	if(curr==NULL)
+	{
+		printf("%ith block does not exist! :( \n",n);
+		return;
+
+	}
+	// while n is valid:
+	while(count!=n)
+	{
+		// If head of next block points to null and count !=n then block is invalid
+		// (Genesis block)
+		if(curr->link==NULL && count!=n)
+		{
+			printf("Nth block does not exist! :( \n");
+			return;
+		}
+		// Else if valid:
+		else if(count==n)
+			// Stop looping
+			break;
+		// Current becomes next block
+		curr=curr->link;
+		count++;
+	}
+	// Print fore and aft data
+	printf("Before: ");
+		printBlock(curr);
+	curr->blockData=newData;
+	printf("\nAfter: ");
+		printBlock(curr);
+	printf("\n");
+}
+
+//What is this for?
+unsigned char* toString(struct block b)
+{
+	// Creates space in memory the size of next block
+	unsigned char *str=malloc(sizeof(unsigned char)*sizeof(b));
+	// Copy it into a string
+	memcpy(str,&b,sizeof(b));
+	return str;
+	// This is so SHA256 can accept any data fed to it rather than rewrite the SHA256 alg
+}
+
+// Print hash of block (by loc)
+void hashPrinter(unsigned char hash[], int length)
+{
+	for(int i=0;i<length;i++)
+		printf("%02x",hash[i]);
+}
+
+// Compare hash numbers
+int hashCompare(unsigned char *str1,unsigned char *str2)
+{
+	for(int i=0;i<SHA256_DIGEST_LENGTH;i++)
+		if(str1[i]!=str2[i])
+			return 0;
+		return 1;
+}
+
+// Print block and contents??
+// Modified to include other block data (Guest hash, time created and session objectives)
+
+void printBlock(struct block *b)
+{
+	printf("%p\t",b);	
+	hashPrinter(b->prevHash,sizeof(b->prevHash));
+	printf("Block hash: \t[%d]\t \n",b->blockData);
+	printf("Block contents:\n");
+	printf("Guest hash: %u \n", b->guestHashID);
+	printf("Time block created: %lld \n", b->currentTime);
+	printf("Session Objectives: %u \n", b->sessionObjectives)
+	printf("%p\n",b->link);
+
+}
+
+// Print all
+void printAllBlocks()
+{
+	struct block *curr=head;
+	int count=0;
+	while(curr)
+	{
+		printBlock(curr);
+		curr=curr->link;
+	}
+
+}
+
+// ==========================================
+// =				TO DO 					=
+// ==========================================
+/*
+	-Build session objectives code
+		-malloc(sizeof(char)*length)
+			-dont forget to use free(string)
+			-
+*/
+
+// Main function
+// Switch statements to let user select functions
+// This needs rewriting to allow for radio-triggered blockchain interactions instead
+void main()
+{
+	int c,n,r;
+	printf("1)addBlock\n2)add n random blocks\n3)alter nth block\n4)print all blocks\n5)verify chain\n");
+	while(1)
+	{
+		printf("Choice: ");
+		scanf("%d",&c);
+		switch(c)
+		{
+			case 1:
+				printf("Enter data: ");
+				scanf("%d",&n);
+				addBlock(n);
+				break;
+			case 2:
+				printf("How Many blocks to enter?: ");
+				scanf("%d",&n);
+				for(int i=0; i<n;i++)
+				{
+					r=rand()%(n*10);
+					printf("Entering: %d\n",r);
+					addBlock(r);
+				}
+				break;
+			case 3:
+				printf("Which block to alter?: ");
+				scanf("%d",&n);
+				printf("Enter value: ");
+				scanf("%d",&r);
+				alterNthBlock(n,r);
+				break;
+			case 4:
+				printAllBlocks();
+				break;
+			case 5:
+				verifyChain();
+				break;
+			default:
+				printf("Bad choice! :((((\n");
+				break
+
+		}
+	}
+}
+
+
+// gcc blockChain2.c -o blockChain.c.o -lcrypto
+// ./blockChain.c.o
+
+// Made with help from: https://www.youtube.com/watch?v=1O-XnbRYJHM
+
+
+
+
+
